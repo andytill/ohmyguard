3f06cf3953d6474e60304f249f2b07bbd3cb7c65
andytill <kittyburrito@googlemail.com>
2015-01-11 19:57:19 +0000

    supports all types, added eunits.

--- src/ohmyguard.erl
+++ src/ohmyguard.erl
@@ -30,11 +30,11 @@
 %%  {eof,7}]
 
 parse_transform(AST_in, _Options) -> 
-%   io:format("== AST ==~n~p~n== AST ==~n~n", [AST_in]),
+    io:format("== AST ==~n~p~n== AST ==~n~n", [AST_in]),
 
     AST_in_1 = [ast(A) || A <- AST_in],
     
-%   io:format("~n== New AST ==~n~p~n== New AST ==~n~n", [AST_in_1]),
+    io:format("~n== New AST ==~n~p~n== New AST ==~n~n", [AST_in_1]),
 
     AST_in_1.
 
@@ -44,10 +44,12 @@ ast({function, _, _, _, Clauses_0} = Func_AST) ->
 ast(Other_AST) ->
     Other_AST.
 
-transform_clause({clause, N, Args, Guards, Body} = _Clause) ->
+transform_clause({clause, N, Args, Guards, Body}) when length(Args) > 0 ->
     {Args_1, Types} = lists:mapfoldl(fun transform_args/2, [], Args),
     Types_Guards = [type_guard(N, T) || T <- Types],
-    {clause, N, Args_1, [Types_Guards | Guards], Body}.
+    {clause, N, Args_1, [Types_Guards | Guards], Body};
+transform_clause(Clause) ->
+    Clause.
 
 transform_args({var, _, _} = Var, Acc) ->
     {Var, Acc};
@@ -57,4 +59,18 @@ transform_args({op, N, '/', {var, N, Var_name} = Arg, {atom, N, Var_type}}, Acc)
 type_guard(N, {Name, Type}) ->
     {call, N, {atom, N, type_to_guard(Type)}, [{var, N, Name}]}.
 
-type_to_guard(binary) -> is_binary.
+%% all the types that are allowed after the the slash as a guard.
+type_to_guard(atom)      -> is_atom;
+type_to_guard(binary)    -> is_binary;
+type_to_guard(bitstring) -> is_bitstring;
+type_to_guard(float)     -> is_float;
+type_to_guard(function)  -> is_function;
+type_to_guard(integer)   -> is_integer;
+type_to_guard(list)      -> is_list;
+type_to_guard(map)       -> is_map;
+type_to_guard(number)    -> is_number;
+type_to_guard(pid)       -> is_pid;
+type_to_guard(port)      -> is_port;
+type_to_guard(reference) -> is_reference;
+type_to_guard(ref)       -> is_reference;
+type_to_guard(tuple)     -> is_tuple.
--- test/omg_simplest.erl
+++ test/omg_simplest.erl
@@ -2,7 +2,63 @@
 
 -compile({parse_transform, ohmyguard}).
 
--export([omg_1/1]).
+-export([omg_binary/1]).
 
-omg_1(X/binary) -> 
-	X.
+-include_lib("eunit/include/eunit.hrl").
+
+%% =============================================================================%%
+%%
+%% =============================================================================
+
+omg_atom(X/atom) -> X.
+omg_binary(X/binary) -> X.
+omg_bitstring(X/bitstring) -> X.
+omg_float(X/float) -> X.
+omg_function(X/function) -> X.
+omg_integer(X/integer) -> X.
+omg_list(X/list) -> X.
+omg_map(X/map) -> X.
+omg_number(X/number) -> X.
+omg_pid(X/pid) -> X.
+omg_port(X/port) -> X.
+omg_reference(X/reference) -> X.
+omg_ref(X/ref) -> X.
+omg_tuple(X/tuple) -> X.
+
+%% =============================================================================
+%% tests
+%% =============================================================================
+
+omg_atom_test()      -> ?assertEqual(derp, omg_atom(derp)).
+omg_binary_test()    -> ?assertEqual(<<"hello">>, omg_binary(<<"hello">>)).
+omg_bitstring_test() -> ?assertEqual(<<4:1,4:6>>, omg_bitstring(<<4:1,4:6>>)).
+omg_float_test()     -> ?assertEqual(3.5, omg_float(3.5)).
+omg_function_test()  -> F = fun() -> ok end,
+                        ?assertEqual(F, omg_function(F)).
+omg_integer_test()   -> ?assertEqual(8, omg_integer(8)).
+omg_list_test()      -> ?assertEqual([1,3,4], omg_list([1,3,4])).
+omg_map_test()       -> ?assertEqual(#{ n => 2 }, omg_map(#{ n => 2 })).
+omg_number_test()    -> ?assertEqual(2, omg_number(2)).
+omg_pid_test()       -> P = spawn(fun() -> ok end),
+                        ?assertEqual(P, omg_pid(P)).
+% omg_port_test()      -> ?assertEqual(derp, omg_port(derp)). TODO
+omg_reference_test() -> Ref = make_ref(),
+                        ?assertEqual(Ref, omg_reference(Ref)).
+omg_ref_test()       -> Ref = make_ref(),
+                        ?assertEqual(Ref, omg_ref(Ref)).
+omg_tuple_test()     -> ?assertEqual({}, omg_tuple({})).
+
+omg_not_atom_test()      -> ?assertException(error, function_clause, omg_atom(1)).
+omg_not_binary_test()    -> ?assertException(error, function_clause, omg_binary(1)).
+omg_not_bitstring_test() -> ?assertException(error, function_clause, omg_bitstring(1)).
+omg_not_float_test()     -> ?assertException(error, function_clause, omg_float(1)).
+omg_not_function_test()  -> ?assertException(error, function_clause, omg_function(1)).
+omg_not_integer_test()   -> ?assertException(error, function_clause, omg_integer(derp)).
+omg_not_list_test()      -> ?assertException(error, function_clause, omg_list(1)).
+omg_not_map_test()       -> ?assertException(error, function_clause, omg_map(1)).
+omg_not_number_test()    -> ?assertException(error, function_clause, omg_number(derp)).
+omg_not_pid_test()       -> ?assertException(error, function_clause, omg_pid(1)).
+omg_not_port_test()      -> ?assertException(error, function_clause, omg_port(1)).
+omg_not_reference_test() -> ?assertException(error, function_clause, omg_reference(1)).
+omg_not_ref_test()       -> ?assertException(error, function_clause, omg_ref(1)).
+omg_not_tuple_test()     -> ?assertException(error, function_clause, omg_tuple(1)).
